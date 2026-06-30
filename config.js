ce995444287f5ca176152c3850e226a95abfa0d1
pathojenzzz <jenznielsen.agujitas@gmail.com>
2026-06-30 16:21:02 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
 window.RESEARCH_FORM_CONFIG = {
-  appsScriptUrl: ""
+  appsScriptUrl: "```js
+window.RESEARCH_FORM_CONFIG = {
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbxd3Vf6-komkq48mrWb_uW__sUkuc6ePkBRJyLzN3wBkET8Wna6oIaqVcBXmOqcMW9DXQ/exec"
+};
+```
+"
 };
